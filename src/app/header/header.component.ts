4443dce05ad87ee5cff20f3863a73b3f6e05850a
brgomesz <bruno.gomes@martinellilabs.com.br>
2024-10-12 20:05:50 -0300

    Adição das telas internas dos dentistas + tela de cadastro

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from '../app.component';
+import { RecepcaoComponent } from '../recepcao/recepcao.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, AppComponent],
+  imports: [CommonModule, AppComponent, RecepcaoComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
--- src/app/recepcao/recepcao.component.ts
+++ src/app/recepcao/recepcao.component.ts
@@ -12,9 +12,68 @@ import { BrowserModule } from '@angular/platform-browser';
   styleUrl: './recepcao.component.scss',
 })
 export class RecepcaoComponent {
-   showRecepcao: string = '1';
+  showRecepcao: string = '3';
+  showDentista: number | null = null;
+  idDentista: string = '0';
+  dentistaSelecionado: any = 0;
 
-  toggleRecepcao() {
-    this.showRecepcao = '1';
+  // toggleRecepcao() {
+  //   this.showRecepcao = '1';
+  // }
+
+  checaVariavel() {
+    console.log(this.showDentista);
   }
+
+  setShowDentista(dentistaId: number) {
+    this.showDentista = dentistaId;
+    console.log('Id do show dentista agora é: ', this.showDentista);
+    this.dentistaSelecionado = this.dentistas.find(
+      (dentista) => dentista.id === dentistaId
+    );
+    console.log('Dentista selecionado:', this.dentistaSelecionado);
+  }
+
+  exibirId(showDentista: number) {
+    console.log('ID do dentista clicado:', showDentista);
+  }
+
+  dentistas = [
+    {
+      id: 1,
+      nomeClinica: 'Clinica Dentes Brancos',
+      nomeDentista: 'Dentista Rodrigo Schultz',
+      especialidade: 'Especialista em implantes dentários',
+      descricaoCompleta:
+        'Esse dentista é especializado em tal coisa e tem um consultorio completo que recebe pácientes de tos os lugares da cidade e possui estacionamentto gratis com ar condicionado e espaço kids',
+      avaliacao: '4/5',
+    },
+    {
+      id: 2,
+      nomeClinica: 'Clinica Vasconcelos',
+      nomeDentista: 'Dentista Adriana Manaus',
+      especialidade: 'Especialista em Aparelhos Ortodônticos',
+      descricaoCompleta:
+        'Esse dentista é especializado em tal coisa e tem um consultorio completo que recebe pácientes de tos os lugares da cidade e possui estacionamentto gratis com ar condicionado e espaço kids',
+      avaliacao: '3/5',
+    },
+    {
+      id: 3,
+      nomeClinica: 'Clinica Sorriso',
+      nomeDentista: 'Dentista Elenir da Rocha',
+      especialidade: 'Especialista em Dentes Quebrados',
+      descricaoCompleta:
+        'Esse dentista é especializado em tal coisa e tem um consultorio completo que recebe pácientes de tos os lugares da cidade e possui estacionamentto gratis com ar condicionado e espaço kids',
+      avaliacao: '5/5',
+    },
+    {
+      id: 4,
+      nomeClinica: 'Clinica Tristeza',
+      nomeDentista: 'Dentista Joana da Silva',
+      especialidade: 'Especialista em Limpeza Dental',
+      descricaoCompleta:
+        'Esse dentista é especializado em tal coisa e tem um consultorio completo que recebe pácientes de tos os lugares da cidade e possui estacionamentto gratis com ar condicionado e espaço kids',
+      avaliacao: '1/5',
+    },
+  ];
 }
